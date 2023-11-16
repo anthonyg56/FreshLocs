6094f5060c0c5d31afd1489dbc6126b57c8bad5c
anthony <devgayflor@gmail.com>
2023-11-16 14:19:56 -0500

    testing

--- src/components/home/Section.tsx
+++ src/components/home/Section.tsx
@@ -10,13 +10,14 @@ export default function Header() {
       return 'h-screen'
     }
 
-    return 'h-[' + screenSize.height + 'px]'
+    if (screenSize.width)
+      return 'h-[' + screenSize.height + 'px]'
   }
   return (
     <div className={`bg-home-bg ${calcHeight()} bg-[80%_0%] bg-no-repeat bg-cover bg-opacity-40 flex items-center justify-center lg:justify-start`}>
         <div className='md:pl-7 lg:pl-14 tracking-wider z-[1000] text-center md:text-start'>
           <h1 className='font-extrabold '>A Full Service Salon</h1>
-          <h1 className='font-extrabold'><span className='text-primary-red'>Specialized</span></h1>
+          <h1 className='font-extrabold'><span className='text-primary-red'>{screenSize.height}</span></h1>
           <h1 className='ont-extrabold'>in Grooming Locs</h1>
           <a href="https://www.vagaro.com/freshlocslocshopllc" ><button className='button-standard mt-5'>Schedule An Appointment</button></a>
         </div>
