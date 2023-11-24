90079cffe1f2ccffa751ae77f7aaf8252fec8651
anthony <devgayflor@gmail.com>
2023-11-23 21:05:22 -0500

    my dumb self forgot to add 100 to the svh

--- src/components/layout/DragHandle.tsx
+++ src/components/layout/DragHandle.tsx
@@ -25,7 +25,7 @@ export default function DragHandle(props: Props) {
     return 'h-[' + screenSize?.height + 'px]'
   }
   return (
-    <div className={`h-[svh] ${isOpen ? "fixed" : "hidden"} top-0 lg:hidden w-full z-[1001] bg-[rgba(0,0,0,0.7)]`} onClick={() => close()} >
+    <div className={`h-[100svh] ${isOpen ? "fixed" : "hidden"} top-0 lg:hidden w-full z-[1001] bg-[rgba(0,0,0,0.7)]`} onClick={() => close()} >
       <div className='max-h-[80%] bg-white h-auto absolute bottom-0 text-black w-full text-center rounded-[16px_16px_0px_0px] px-5 pb-5 overflow-y-scroll'>
         <div className='flex justify-center pt-5 pb-7'>
           <span className='h-[5px] w-[32px] block bg-gray-600 rounded-full' onClick={() => close()}></span>
